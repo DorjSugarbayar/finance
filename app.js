34252c172c2b667eeaa614dc39d3a040417de3dd
Sugarbayar <dorjsugaraaz@gmail.com>
2022-04-14 10:10:15 +0800

    sanhuugiin modul ruu orlogo zarlagiig hadgalah

--- app.js
+++ app.js
@@ -1,27 +1,108 @@
 // Дэлгэцтэй ажиллах контроллер
-var uiController = (function() {})();
+var uiController = (function() {
+
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn"
+  };
+
+  return {
+    getInput: function(){
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      }
+    },
+
+    getDOMstrings: function(){
+      return DOMstrings;
+    },
+  }
+})();
 
 // Санхүүтэй ажиллах контроллер
-var financeController = (function() {})();
+var financeController = (function() {
+  var Income = function(id, description, value){
+    this.id = id;
+    this.description = this.description;
+    this.value = value;
+  }
+
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = this.description;
+    this.value = value;
+  };
+
+  var data = {
+    items : {
+      inc: [],
+      exp: []
+    },
+
+    totals: {
+      inc: 1000,
+      exp: 500
+    }
+  };
+  
+  return {
+    addItem: function(type, desc, val){
+      
+      var item, id;
+
+      if (data.items[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+
+      if(type === 'inc'){
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val)
+      }
+
+      data.items[type].push(item);
+    }
+  }
+})();
 
 // Програмын холбогч контроллер
 var appController = (function(uiController, financeController) {
+
+  var DOM = uiController.getDOMstrings();
+
   var ctrlAddItem = function() {
     // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
-    console.log("Дэлгэцээс өгөгдлөө авах хэсэг");
+    var input = uiController.getInput();
     // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    financeController.addItem(input.type, input.description, input.value);
     // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
     // 4. Төсвийг тооцоолно
     // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
   };
 
-  document.querySelector(".add__btn").addEventListener("click", function() {
-    ctrlAddItem();
-  });
-
-  document.addEventListener("keypress", function(event) {
-    if (event.keyCode === 13 || event.which === 13) {
+  var setupEventListeners = function() {
+    document.querySelector(DOM.addBtn).addEventListener("click", function() {
       ctrlAddItem();
+    });
+  
+    document.addEventListener("keypress", function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  }
+
+  return {
+    init: function() {
+      console.log("App Started");
+      setupEventListeners();
     }
-  });
+  }
 })(uiController, financeController);
+
+appController.init();
