3e119cc9c32e0628a5a2b2ffbe7a04870e7e44e6
Sugarbayar <dorjsugaraaz@gmail.com>
2022-04-14 11:33:52 +0800

    Orlogo zarlagiig nemehed delgetsend gargah

--- app.js
+++ app.js
@@ -20,6 +20,25 @@ var uiController = (function() {
     getDOMstrings: function(){
       return DOMstrings;
     },
+
+    addListItem: function(item, type){
+      var html, list;
+      if(type==='inc') {
+        console.log("inc");
+        console.log(item);
+        list = '.income__list';
+        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      } else {
+        console.log("exp");
+        list = '.expenses__list';
+        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      }
+
+      html = html.replace('%id%', item.id);
+      html = html.replace('$$DESCRIPTION$$', item.description);
+      html = html.replace('$$VALUE$$', item.value);
+      document.querySelector(list).insertAdjacentHTML("beforeend", html);
+    }
   }
 })();
 
@@ -27,13 +46,15 @@ var uiController = (function() {
 var financeController = (function() {
   var Income = function(id, description, value){
     this.id = id;
-    this.description = this.description;
+    this.description = description;
     this.value = value;
+    console.log("qweqrqewr"+description);
+
   }
 
   var Expense = function(id, description, value) {
     this.id = id;
-    this.description = this.description;
+    this.description = description;
     this.value = value;
   };
 
@@ -53,7 +74,7 @@ var financeController = (function() {
     addItem: function(type, desc, val){
       
       var item, id;
-
+      console.log("SSSSSSSSSS" + desc);
       if (data.items[type].length === 0) id = 1;
       else {
         id = data.items[type][data.items[type].length - 1].id + 1;
@@ -66,6 +87,10 @@ var financeController = (function() {
       }
 
       data.items[type].push(item);
+      console.log("PPPPPPPPPPP");
+      console.log(item);
+      console.log("PPPPPPPPPPP");
+      return item;
     }
   }
 })();
@@ -79,8 +104,12 @@ var appController = (function(uiController, financeController) {
     // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
     var input = uiController.getInput();
     // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
-    financeController.addItem(input.type, input.description, input.value);
+    var item = financeController.addItem(input.type, input.description, input.value);
+    console.log("QQQQQQQQQQQQ");
+    console.log(item);
+    console.log("QQQQQQQQQQQQ");
     // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
+    uiController.addListItem(item, input.type);
     // 4. Төсвийг тооцоолно
     // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
   };
